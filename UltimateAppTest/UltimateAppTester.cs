556f256e1a0930c32d113ece7b6f15d9ffe21226
sznyari <nyszabi.snr@gmail.com>
2022-04-29 11:12:33 +0200

    Extended Test Cases

--- UltimateAppTest/UltimateAppTester.cs
+++ UltimateAppTest/UltimateAppTester.cs
@@ -11,5 +11,29 @@ namespace UltimateApp.Test
             int? retVal = Calculator.calculate(2, 3, Operation.Add);
             Assert.AreEqual(5, retVal);
         }
+        [TestMethod]
+        public void SubtractTest()
+        {
+            int? retVal = Calculator.calculate(5, 3, Operation.Subtract);
+            Assert.AreEqual(2, retVal);
+        }
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Multiply);
+            Assert.AreEqual(6, retVal);
+        }
+        [TestMethod]
+        public void DivideTest()
+        {
+            int? retVal = Calculator.calculate(10, 5, Operation.Divide);
+            Assert.AreEqual(2, retVal);
+        }
+        [TestMethod]
+        public void ModuloTest()
+        {
+            int? retVal = Calculator.calculate(4, 3, Operation.Modulo);
+            Assert.AreEqual(1, retVal);
+        }
     }
 }
